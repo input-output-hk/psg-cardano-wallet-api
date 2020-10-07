a846d5929718e44f5da2da21f1bd3ff4e0c4e612
Maciej Bak <maciej.bak.85@gmail.com>
2020-10-07 11:53:11 +0100

    CardanoApiCodecSpec -> TxMetadataCodecSpec, cut not needed jsonFiles decoded

--- src/test/scala/iog/psg/cardano/TxMetadataCodecSpec.scala
+++ src/test/scala/iog/psg/cardano/TxMetadataCodecSpec.scala
@@ -1,60 +1,13 @@
 package iog.psg.cardano
 
-import java.time.ZonedDateTime
-
 import akka.util.ByteString
 import io.circe.syntax.EncoderOps
-import CardanoApiCodec._
-
 import iog.psg.cardano.CardanoApiCodec._
-import iog.psg.cardano.util.{DummyModel, JsonFiles, ModelCompare}
+import iog.psg.cardano.util.DummyModel
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class CardanoApiCodecSpec extends AnyFlatSpec with Matchers with ModelCompare with DummyModel with JsonFiles {
-
-  "Wallet" should "be decoded properly" in {
-    compareWallets(jsonFileWallet, wallet)
-  }
-
-  it should "decode wallet's list" in {
-    jsonFileWallets.size shouldBe 1
-    compareWallets(jsonFileWallets.head, wallet)
-  }
-
-  "network information" should "be decoded properly" in {
-    compareNetworkInformation(
-      jsonFileNetInfo,
-      NetworkInfo(
-        syncProgress = SyncStatus(SyncState.ready, None),
-        networkTip = networkTip.copy(height = None),
-        nodeTip = nodeTip,
-        nextEpoch = NextEpoch(ZonedDateTime.parse("2000-01-02T03:04:05.000Z"), 14)
-      )
-    )
-  }
-
-  "list addresses" should "be decoded properly" in {
-    jsonFileAddresses.size shouldBe 3
-    compareAddress(jsonFileAddresses.head, WalletAddressId(id = addressIdStr, Some(AddressFilter.unUsed)))
-  }
-
-  "list transactions" should "be decoded properly" in {
-    jsonFileCreatedTransactionsResponse.size shouldBe 1
-    compareTransaction(jsonFileCreatedTransactionsResponse.head, createdTransactionResponse)
-  }
-
-  it should "decode one transaction" in {
-    compareTransaction(jsonFileCreatedTransactionResponse, createdTransactionResponse)
-  }
-
-  "estimate fees" should "be decoded properly" in {
-    compareEstimateFeeResponse(jsonFileEstimateFees, estimateFeeResponse)
-  }
-
-  "fund payments" should "be decoded properly" in {
-    compareFundPaymentsResponse(jsonFileCoinSelectionRandom, fundPaymentsResponse)
-  }
+class TxMetadataCodecSpec extends AnyFlatSpec with Matchers with DummyModel {
 
   "TxMetadataMapIn encode" should "encode string value to proper json" in  {
     val map: Map[Long, MetadataValue] = Map(0L -> MetadataValueStr("cardano"))
--- src/test/scala/iog/psg/cardano/util/JsonFiles.scala
+++ src/test/scala/iog/psg/cardano/util/JsonFiles.scala
@@ -1,7 +1,6 @@
 package iog.psg.cardano.util
 
 import io.circe.Decoder
-import io.circe.generic.auto._
 import io.circe.parser.decode
 import iog.psg.cardano.CardanoApiCodec._
 import org.scalatest.Assertions
@@ -11,13 +10,7 @@ import scala.io.Source
 trait JsonFiles { self: Assertions =>
 
   final lazy val jsonFileWallet = decodeJsonFile[Wallet]("wallet.json")
-  final lazy val jsonFileWallets = decodeJsonFile[Seq[Wallet]]("wallets.json")
-  final lazy val jsonFileNetInfo = decodeJsonFile[NetworkInfo]("netinfo.json")
-  final lazy val jsonFileAddresses = decodeJsonFile[Seq[WalletAddressId]]("addresses.json")
   final lazy val jsonFileCreatedTransactionResponse = decodeJsonFile[CreateTransactionResponse]("transaction.json")
-  final lazy val jsonFileCreatedTransactionsResponse = decodeJsonFile[Seq[CreateTransactionResponse]]("transactions.json")
-  final lazy val jsonFileCoinSelectionRandom = decodeJsonFile[FundPaymentsResponse]("coin_selections_random.json")
-  final lazy val jsonFileEstimateFees = decodeJsonFile[EstimateFeeResponse]("estimate_fees.json")
 
   final def getJsonFromFile(file: String): String = {
     val source = Source.fromURL(getClass.getResource(s"/jsons/$file"))
