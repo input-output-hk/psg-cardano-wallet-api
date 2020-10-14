5838fed0aaceddcafb5e2e78e7c271969506b151
Maciej Bak <maciej.bak.85@gmail.com>
2020-10-14 14:28:44 +0100

    cmdline.sh use newest jar

--- cmdline.sh
+++ cmdline.sh
@@ -5,4 +5,5 @@ VER=0.1.3-SNAPSHOT
 #BASE_URL="http://localhost:8090/v2/"
 
 #run sbt assembly to create this jar
-exec java -jar target/scala-2.13/psg-cardano-wallet-api-assembly-${VER}.jar -baseUrl ${BASE_URL} "$@"
+NEWEST_JAR_NAME=`ls target/scala-2.13/ -Frt | grep '.jar' | tail -n 1`
+exec java -jar target/scala-2.13/${NEWEST_JAR_NAME} -baseUrl ${BASE_URL} "$@"
