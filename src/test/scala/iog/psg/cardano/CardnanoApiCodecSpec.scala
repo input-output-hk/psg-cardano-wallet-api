057a4a21e478551b7adb0342ed57bdda3fed7d27
Maciej Bak <maciej.bak.85@gmail.com>
2020-10-15 13:10:36 +0100

    nullable codecs fixes.

---
+++ src/test/scala/iog/psg/cardano/CardnanoApiCodecSpec.scala
@@ -0,0 +1,36 @@
+package iog.psg.cardano
+
+import io.circe.syntax.EncoderOps
+import iog.psg.cardano.CardanoApiCodec._
+import iog.psg.cardano.util.DummyModel
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class CardnanoApiCodecSpec extends AnyFlatSpec with Matchers with DummyModel {
+
+  "Encode class with Nones" should "drop nulls in Delegation" in  {
+    val delAct = DelegationActive(
+      status = DelegationStatus.delegating,
+      target = None
+    )
+    val delNext = DelegationNext(
+      status = DelegationStatus.delegating,
+      changesAt = None
+    )
+    val del = Delegation(delAct, List(delNext))
+    del.asJson.noSpaces shouldBe """{"active":{"status":"delegating"},"next":[{"status":"delegating"}]}""".stripMargin
+  }
+
+  it should "drop nulls in NetworkTip" in {
+    val networkTip = NetworkTip(
+      epochNumber = 1, slotNumber = 2, height = None, absoluteSlotNumber = None
+    )
+    networkTip.asJson.noSpaces shouldBe """{"epoch_number":1,"slot_number":2}"""
+  }
+
+  it should "drop nulls in NodeTip" in {
+    val nt = nodeTip.copy(absoluteSlotNumber = None)
+    nt.asJson.noSpaces shouldBe """{"height":{"quantity":1337,"unit":"block"},"slot_number":1337,"epoch_number":14}"""
+  }
+
+}
