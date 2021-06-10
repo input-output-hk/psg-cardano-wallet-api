b0023880dde4d1a6e25cf87ffa2f06a23b91295f
mcsherrylabs <alan@mcsherrylabs.com>
2021-06-10 13:11:04 +0100

    Hack.

--- src/it/scala/iog/psg/cardano/CantApiSpec.scala
+++ src/it/scala/iog/psg/cardano/CantApiSpec.scala
@@ -5,6 +5,10 @@ import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
+import java.io.File
+import java.nio.charset.StandardCharsets
+import java.nio.file.{Files, Paths}
+
 class CantApiSpec
     extends AnyFlatSpec
     with Matchers
@@ -13,8 +17,20 @@ class CantApiSpec
     with CustomPatienceConfiguration {
 
   "Dummy" should "do it" in {
+
+    println(ConfigureFactory.config.getString("cardano.wallet1.mnemonic"))
+    println(ConfigureFactory.config.getString("cardano.wallet2.mnemonic"))
+    println(ConfigureFactory.config.getString("cardano.wallet3.mnemonic"))
+    println(ConfigureFactory.config.getString("cardano.wallet3.mnemonicsecondary"))
+
+    ConfigureFactory.config.getString("cardano.wallet1.mnemonic").split(" ").foreach(println)
     val t = ConfigureFactory.config.getConfig("cardano")
-    println(t)
+    val f = new File("crap.txt")
+    Files.write(Paths.get("crap.txt"), t.toString.getBytes(StandardCharsets.UTF_8))
+    assert(f.exists(), "where is the file?")
+
+    ConfigureFactory.config.getString("cardano.wallet1.mnemonic")
+    println(f.getPath)
   }
 
 }
