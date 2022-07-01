6ecbfefa4bb791b9cf8fd71916457bc5a1afde26
michal <michal.skowron@iohk.io>
2022-07-01 12:30:57 +0200

    feat(psgs-638): extend nft metadata model with optional fields from cip-25

--- src/main/scala/iog/psg/cardano/experimental/cli/model/MetadataJsonFile.scala
+++ src/main/scala/iog/psg/cardano/experimental/cli/model/MetadataJsonFile.scala
@@ -1,7 +1,11 @@
 package iog.psg.cardano.experimental.cli.model
 
+import akka.http.javadsl.model.Uri
+import akka.http.scaladsl.model.MediaType
 import cats.data.NonEmptyList
 import io.circe._
+import io.circe.generic.extras.Configuration
+import io.circe.generic.semiauto.deriveEncoder
 import io.circe.syntax._
 import iog.psg.cardano.experimental.cli.api.InFile
 import iog.psg.cardano.experimental.cli.util.RandomTempFolder
@@ -13,7 +17,19 @@ sealed trait MetadataJson extends InFile
 
 object MetadataJson {
 
-  case class Nft(label: String, name: String, image: String)
+  case class Nft(label: String,
+                 name: String,
+                 image: Seq[String],
+                 mediaType: Option[String],
+                 description: Seq[String],
+                 files: Seq[NftFile])
+
+  object Nft {
+    def apply(label: String, name: String, image: String) =
+      new Nft(label = label, name = name, image = Seq(image), mediaType = None, description = Seq.empty, files = Seq.empty)
+  }
+
+  case class NftFile(name: String, mediaType: String, source: Seq[String])
 
   case class NftMetadataJson (
                                policyId: PolicyId,
@@ -24,6 +40,8 @@ object MetadataJson {
 
   def asString(metadataJson: MetadataJson): String = metadataJson.asJson.noSpaces
 
+  implicit val nftFileEncoder: Encoder[NftFile] = deriveEncoder
+
   implicit val encoder: Encoder[MetadataJson] = {
 
     case NftMetadataJson(policyId: PolicyId, nfts) =>
@@ -32,10 +50,14 @@ object MetadataJson {
         (policyId.value, Json.obj(nfts.map(nft =>
           (nft.label, Json.obj(
             ("name", nft.name.asJson),
-            ("image", nft.image.asJson)
-          ))):_*
+            ("image", if(nft.image.size > 1) nft.image.asJson else nft.image.head.asJson),
+            ("mediaType", nft.mediaType.asJson),
+            ("description", nft.description.asJson),
+            ("files", if(nft.files.size == 1) nft.files.head.asJson else nft.files.asJson)
+          ).deepDropNullValues.dropEmptyValues)):_*
         )
         )
       )))
   }
+
 }
